5796be709b72724c5cfd2eec981f6169fd2efc2a
jeongho218 <82637024+jeongho218@users.noreply.github.com>
2023-11-20 22:53:53 +0900

    workspaces.service.spec.ts 테스트 파일에 mocking 레포지터리 등록 나중에 유닛 테스트 연습할때 사용할 것

--- a-nest/src/workspaces/workspaces.service.spec.ts
+++ a-nest/src/workspaces/workspaces.service.spec.ts
@@ -1,12 +1,46 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { WorkspacesService } from './workspaces.service';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Users } from '../entities/Users';
+import { ChannelMembers } from '../entities/ChannelMembers';
+import { WorkspaceMembers } from '../entities/WorkspaceMembers';
+import { Channels } from '../entities/Channels';
+import { Workspaces } from '../entities/Workspaces';
+
+class MockWorkspacesRepository {}
+class MockChannelsRepository {}
+class MockWorkspaceMembersRepository {}
+class MockChannelMembersRepository {}
+class MockUsersRepository {}
 
 describe('WorkspacesService', () => {
   let service: WorkspacesService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [WorkspacesService],
+      providers: [
+        WorkspacesService,
+        {
+          provide: getRepositoryToken(Users),
+          useClass: MockUsersRepository,
+        },
+        {
+          provide: getRepositoryToken(ChannelMembers),
+          useClass: MockChannelMembersRepository,
+        },
+        {
+          provide: getRepositoryToken(WorkspaceMembers),
+          useClass: MockWorkspaceMembersRepository,
+        },
+        {
+          provide: getRepositoryToken(Channels),
+          useClass: MockChannelsRepository,
+        },
+        {
+          provide: getRepositoryToken(Workspaces),
+          useClass: MockWorkspacesRepository,
+        },
+      ],
     }).compile();
 
     service = module.get<WorkspacesService>(WorkspacesService);
